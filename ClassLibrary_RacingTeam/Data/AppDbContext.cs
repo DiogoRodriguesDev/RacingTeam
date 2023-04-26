9862d6454582a197581f195f24578b1bb49fc18c
Diogo Rodrigues <diogo.rodrigues@slicept.onmicrosoft.com>
2023-04-26 16:01:06 +0100

    Criação da Pasta Data  com as classes AppDbContext e IDataContext

---
+++ ClassLibrary_RacingTeam/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary_RacingTeam.Models;
+
+namespace ClassLibrary_RacingTeam.Data
+{
+    public class AppDbContext : DbContext, IDataContext
+    {
+        public DbSet<Carro> Carros { get; set; }
+        public DbSet<Circuito> Circuitos { get; set; }
+        public DbSet<ClassificacaoGeral> Classificacoes { get; set; }
+        public DbSet<Competicao> Competicoes { get; set; }
+        public DbSet<Corrida> Corridas { get; set; }
+        public DbSet<Equipa> Equipas { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Resultado> Resultados { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Data Source=DIO-PC\MSSQL_SERVER_DIO;Initial Catalog=AppDataBase; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ ClassLibrary_RacingTeam/Data/IDataContext.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary_RacingTeam.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary_RacingTeam.Data
+{
+    public interface IDataContext
+    {
+        DbSet<Carro> Carros { get; set; }
+        DbSet<Circuito> Circuitos { get; set; }
+        DbSet<ClassificacaoGeral> Classificacoes { get; set; }
+        DbSet<Competicao> Competicoes { get; set; }
+        DbSet<Corrida> Corridas { get; set; }
+        DbSet<Equipa> Equipas { get; set; }
+        DbSet<Piloto> Pilotos { get; set; }
+        DbSet<Resultado> Resultados { get; set; }
+        int SaveChanges();
+        Task<int> SaveChangesAsync(CancellationToken cancellation = default);
+    }
+}
